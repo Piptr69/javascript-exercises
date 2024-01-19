e1a04b40ec88f7fb53d80fdc93d974210137847f
Piptr69 <p.paluchh02@gmail.com>
2024-01-19 18:05:25 +0100

    remove element from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, index) {
+    let newArray;
+    for(let i = 0; i < index; i++)
+    {
+        newArray[i] = numbers[i];
+    }
+    let newArray2;
+    let arrayLength = numbers.length;
+    for(let i = numbers.length; i > index; i++)
+    {
+        newArray2[i] = numbers[i];
+    }
+    return newArray.concat(newArray2);
 };
 
 // Do not edit below this line
