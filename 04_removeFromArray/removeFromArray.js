6516c48ee6372e080fc3be59f7ac779c59227f30
Piptr69 <p.paluchh02@gmail.com>
2024-01-19 18:29:32 +0100

    fix numbers.length var

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,6 @@
 const removeFromArray = function(numbers, index) {
     let newArray = [];
-    for (let i = 0, n = numberslength; i < n; i++)
+    for (let i = 0, n = numbers.length; i < n; i++)
     {
         if (i != index - 1)
         {
