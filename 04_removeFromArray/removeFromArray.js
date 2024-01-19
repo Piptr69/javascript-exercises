4d79532bf5acc34762065c16a046f6c052281c8d
Piptr69 <p.paluchh02@gmail.com>
2024-01-19 18:28:41 +0100

    rework whole thing

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,16 +1,13 @@
 const removeFromArray = function(numbers, index) {
     let newArray = [];
-    for(let i = 0; i < index - 1; i++)
+    for (let i = 0, n = numberslength; i < n; i++)
     {
-        newArray[i] = numbers[i];
+        if (i != index - 1)
+        {
+            newArray.push(numbers[i]);
+        }
     }
-    let newArray2 = [];
-    let arrayLength = numbers.length;
-    for(let i = index; i < arrayLength + 1; i++)
-    {
-        newArray2[i] = numbers[i];
-    }
-    return newArray.concat(newArray2);
+    return newArray;
 };
 
 // Do not edit below this line
